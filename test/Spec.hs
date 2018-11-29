d57c35b9f96d206651e1966241b17c68651d2330
Yair Chuchem <yairchu@gmail.com>
2018-11-29 18:23:38 +0200

    simpler kinds

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE KindSignatures, TypeFamilies, UndecidableInstances, UndecidableSuperClasses, MultiParamTypeClasses, FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies, UndecidableInstances, UndecidableSuperClasses, MultiParamTypeClasses, FlexibleContexts #-}
 
 import GHC.Exts (Constraint)
 
-type family ChildrenConstraint (expr :: (* -> *) -> *) (constraint :: ((* -> *) -> *) -> Constraint) :: Constraint
+type family ChildrenConstraint expr (constraint :: * -> Constraint) :: Constraint
 
-class ChildrenConstraint t (UnifyMonad m) => UnifyMonad (m :: * -> *) t where
+class ChildrenConstraint t (UnifyMonad m) => UnifyMonad m t where
 
-newtype Typ f = Typ (f Int)
+newtype Typ = Typ Int
 
 type instance ChildrenConstraint Typ constraint = constraint Typ
 
 -- Replacing `Typ` with `t` doesn't cause GHC to be stuck
-t :: UnifyMonad m Typ => m (t a)
+t :: UnifyMonad m Typ => (m, t)
 t = undefined
