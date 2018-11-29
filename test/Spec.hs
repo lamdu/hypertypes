6a71d021871004ac25b11b7267a49e570392fcb7
Yair Chuchem <yairchu@gmail.com>
2018-11-29 18:21:22 +0200

    remove Children class

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,15 +2,13 @@
 
 import GHC.Exts (Constraint)
 
-class ChildrenConstraint expr Children => Children expr where
-    type ChildrenConstraint expr (constraint :: ((* -> *) -> *) -> Constraint) :: Constraint
+type family ChildrenConstraint (expr :: (* -> *) -> *) (constraint :: ((* -> *) -> *) -> Constraint) :: Constraint
 
 class ChildrenConstraint t (UnifyMonad m) => UnifyMonad (m :: * -> *) t where
 
 newtype Typ f = Typ (f Int)
 
-instance Children Typ where
-    type ChildrenConstraint Typ constraint = constraint Typ
+type instance ChildrenConstraint Typ constraint = constraint Typ
 
 -- Replacing `Typ` with `t` doesn't cause GHC to be stuck
 t :: UnifyMonad m Typ => m (t a)
