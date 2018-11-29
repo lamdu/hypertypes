3662219a38416186c1807468951a6a16d8a7276d
Yair Chuchem <yairchu@gmail.com>
2018-11-29 17:47:13 +0200

    making example smaller

--- src/AST.hs
+++ src/AST.hs
@@ -1,55 +1,8 @@
-{-# LANGUAGE NoImplicitPrelude, TypeFamilies, RankNTypes, ConstraintKinds, UndecidableInstances, UndecidableSuperClasses, ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies, UndecidableInstances, UndecidableSuperClasses #-}
 
-module AST
-    ( Node, LeafNode
-    , Children(..), ChildrenWithConstraint
-    , ChildOf, monoChildren
-    , overChildren
-    , hoist
-    ) where
+module AST where
 
-import           Control.Lens (Traversal)
-import           Data.Functor.Const (Const(..))
-import           Data.Functor.Identity (Identity(..))
-import           Data.Proxy (Proxy(..))
-import           GHC.Exts (Constraint)
-
-import           Prelude.Compat
-
-type Node f expr = f (expr f)
-type LeafNode f expr = Node f (Const expr)
+import GHC.Exts (Constraint)
 
 class ChildrenConstraint expr Children => Children expr where
     type ChildrenConstraint expr (constraint :: ((* -> *) -> *) -> Constraint) :: Constraint
-    children ::
-        (Applicative f, ChildrenConstraint expr constraint) =>
-        Proxy constraint ->
-        (forall child. constraint child => Node n child -> f (Node m child)) ->
-        expr n -> f (expr m)
-
-type ChildrenWithConstraint expr constraint = (Children expr, ChildrenConstraint expr constraint)
-
-type family ChildOf (expr :: (* -> *) -> *) :: (* -> *) -> *
-
-monoChildren ::
-    forall expr n m.
-    (ChildrenWithConstraint expr ((~) (ChildOf expr))) =>
-    Traversal (expr n) (expr m) (Node n (ChildOf expr)) (Node m (ChildOf expr))
-monoChildren = children (Proxy :: Proxy ((~) (ChildOf expr)))
-
-overChildren ::
-    ChildrenWithConstraint expr constraint =>
-    Proxy constraint ->
-    (forall child. constraint child => Node n child -> Node m child) ->
-    expr n -> expr m
-overChildren p f = runIdentity . children p (Identity . f)
-
-instance Children (Const val) where
-    type ChildrenConstraint (Const val) constraint = ()
-    children _ _ (Const x) = pure (Const x)
-
-hoist ::
-    (Children expr, Functor f, Functor g) =>
-    (forall a. f a -> g a) ->
-    expr f -> expr g
-hoist f = overChildren (Proxy :: Proxy Children) (f . fmap (hoist f))
--- test/GhcStuck.hs
+++ test/GhcStuck.hs
@@ -1,9 +1,14 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell, FlexibleContexts, TypeFamilies #-}
 
 module GhcStuck where
 
+import AST
 import AST.Unify
-import TypeLang
+
+newtype Typ f = Typ (f Int)
+
+instance Children Typ where
+    type ChildrenConstraint Typ constraint = constraint Typ
 
 -- Replacing `Typ` with `t` doesn't cause GHC to be stuck
 t :: UnifyMonad m Typ => m (t a)
