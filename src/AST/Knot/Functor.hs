9a7ac184538b6f03eb32cb1cdfbc3ed780b65a1b
Yair Chuchem <yairchu@gmail.com>
2018-12-19 16:22:21 +0200

    style

--- src/AST/Knot/Functor.hs
+++ src/AST/Knot/Functor.hs
@@ -9,15 +9,15 @@ import           AST.Knot (Tie)
 import qualified Control.Lens as Lens
 import           Data.Binary
 
-type InToKnot f k = f (Tie k (ToKnot f))
 
 newtype ToKnot f k = ToKnot { getToKnot :: f (Tie k (ToKnot f)) }
 
 Lens.makePrisms ''ToKnot
 
-deriving instance Eq (InToKnot f k) => Eq (ToKnot f k)
-deriving instance Ord (InToKnot f k) => Ord (ToKnot f k)
-deriving instance Show (InToKnot f k) => Show (ToKnot f k)
+type InToKnot f k = f (Tie k (ToKnot f))
+deriving instance Eq     (InToKnot f k) => Eq     (ToKnot f k)
+deriving instance Ord    (InToKnot f k) => Ord    (ToKnot f k)
+deriving instance Show   (InToKnot f k) => Show   (ToKnot f k)
 deriving instance Binary (InToKnot f k) => Binary (ToKnot f k)
 
 makeChildrenRecursive [''ToKnot]
