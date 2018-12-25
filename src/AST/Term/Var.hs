533830c02057028b87944b7159c04fd2be458f0f
Yair Chuchem <yairchu@gmail.com>
2018-12-25 12:52:07 +0200

    Var generalized to support let-generalization

--- src/AST/Term/Var.hs
+++ src/AST/Term/Var.hs
@@ -2,7 +2,7 @@
 
 module AST.Term.Var
     ( Var(..), _Var
-    , HasScopeTypes(..)
+    , MonadScopeTypes(..)
     ) where
 
 import           AST.Class.Infer (MonadInfer, Infer(..), TypeAST)
@@ -11,13 +11,9 @@ import           AST.Class.Recursive.TH (makeChildrenRecursive)
 import           AST.Knot (Knot, Tree)
 import           AST.Unify (Unify, UniVar)
 import           Control.DeepSeq (NFData)
-import           Control.Lens (Lens')
 import           Control.Lens.Operators
 import qualified Control.Lens as Lens
-import           Control.Monad.Reader (MonadReader)
 import           Data.Binary (Binary)
-import           Data.Map (Map)
-import           Data.Maybe (fromMaybe)
 import           GHC.Generics (Generic)
 
 import           Prelude.Compat
@@ -31,23 +27,17 @@ Lens.makePrisms ''Var
 
 makeChildrenRecursive [''Var]
 
-class Ord v => HasScopeTypes v u t env where
-    scopeTypes :: Lens' env (Map v (Tree u t))
-
-instance Ord v => HasScopeTypes v u t (Map v (Tree u t)) where
-    scopeTypes = id
+class MonadScopeTypes v t m where
+    scopeType :: v -> m (Tree (UniVar m) t)
+    localScopeType :: v -> m (Tree (UniVar m) t) -> m a -> m a
 
 type instance TypeAST (Var v t) = TypeAST t
 
 instance
     ( MonadInfer m
     , Recursive (Unify m) (TypeAST expr)
-    , MonadReader env m
-    , HasScopeTypes v (UniVar m) (TypeAST expr) env
+    , MonadScopeTypes v (TypeAST expr) m
     ) =>
     Infer m (Var v expr) where
 
-    infer (Var x) =
-        Lens.view (scopeTypes . Lens.at x)
-        <&> fromMaybe (error "name error")
-        <&> (, Var x)
+    infer (Var x) = scopeType x <&> (, Var x)
