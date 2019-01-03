a1d5952ca05d42da3db7ac619df4aefff866cbc2
Yair Chuchem <yairchu@gmail.com>
2019-01-03 16:17:17 +0200

    add asTree helper

--- src/AST/Knot.hs
+++ src/AST/Knot.hs
@@ -3,9 +3,12 @@
 module AST.Knot
     ( Knot(..), RunKnot
     , Tie, Tree
+    , asTree
     ) where
 
 import Data.Kind (Type)
+
+import Prelude.Compat
 
 newtype Knot = Knot (Knot -> Type)
 
@@ -16,6 +19,14 @@ type family RunKnot (knot :: Knot) = (r :: Knot -> Type) where
 -- * Its inputs are constrained to shape of `'Knot a`
 -- * It is injective (`| r -> knot`), but due to no support for constrained type families it's not expressible atm:
 --   (see https://ghc.haskell.org/trac/ghc/ticket/15691)
+--
+-- Because `RunKnot` can't declared as bijective, uses of it may block inferences.
+-- In those cases wrapping terms with the `asTree` helper makes the type inference
+-- overcome the `RunKnot` block.
 
 type Tree k t = k ('Knot t)
+
+asTree :: Tree p q -> Tree p q
+asTree = id
+
 type Tie knot ast = Tree (RunKnot knot) ast
