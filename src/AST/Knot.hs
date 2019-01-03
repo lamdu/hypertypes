d2b06737865019a355eeeea86e3121821812e7c9
Yair Chuchem <yairchu@gmail.com>
2019-01-03 15:14:19 +0200

    add comment about RunKnot

--- src/AST/Knot.hs
+++ src/AST/Knot.hs
@@ -8,8 +8,14 @@ module AST.Knot
 import Data.Kind (Type)
 
 newtype Knot = Knot (Knot -> Type)
+
 type family RunKnot (knot :: Knot) = (r :: Knot -> Type) where
     RunKnot ('Knot t) = t
+
+-- Notes about `RunKnot`:
+-- * Its inputs are constrained to shape of `'Knot a`
+-- * It is injective (`| r -> knot`), but due to no support for constrained type families it's not expressible atm:
+--   (see https://ghc.haskell.org/trac/ghc/ticket/15691)
 
 type Tree k t = k ('Knot t)
 type Tie knot ast = Tree (RunKnot knot) ast
