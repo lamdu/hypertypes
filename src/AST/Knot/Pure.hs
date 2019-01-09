ed7d2447ec2bf3f9b43a341d6e5113ff9ebd48c3
Yair Chuchem <yairchu@gmail.com>
2019-01-09 13:10:07 +0200

    more instances

--- src/AST/Knot/Pure.hs
+++ src/AST/Knot/Pure.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude, StandaloneDeriving, UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies, TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies, TemplateHaskell, DeriveGeneric #-}
 
 module AST.Knot.Pure
     ( Pure(..), _Pure
@@ -7,18 +7,24 @@ module AST.Knot.Pure
 
 import AST.Class.Children.TH (makeChildren)
 import AST.Knot (Tie)
+import Control.DeepSeq (NFData)
 import Control.Lens (makePrisms)
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
 import Text.PrettyPrint.HughesPJClass (Pretty(..))
 import Text.Show.Combinators ((@|), showCon)
 
 import Prelude.Compat
 
 newtype Pure k = Pure { getPure :: Tie k Pure }
+    deriving Generic
 makePrisms ''Pure
 makeChildren ''Pure
 
 deriving instance Eq  (Tie k Pure) => Eq  (Pure k)
 deriving instance Ord (Tie k Pure) => Ord (Pure k)
+instance Binary (Tie k Pure) => Binary (Pure k)
+instance NFData (Tie k Pure) => NFData (Pure k)
 
 instance Show (Tie k Pure) => Show (Pure k) where
     showsPrec p (Pure x) = (showCon "Pure" @| x) p
