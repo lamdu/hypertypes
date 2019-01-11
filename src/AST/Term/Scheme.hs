a41c16a6a1db5f929fd73fe78df9750f40c44390
Yair Chuchem <yairchu@gmail.com>
2019-01-12 00:36:37 +0200

    Remove AST.Term.Scheme.Load
    Move loadScheme to AST.Term.Scheme
    Other parts belong to the upcoming nominal terms

--- src/AST/Term/Scheme.hs
+++ src/AST/Term/Scheme.hs
@@ -7,6 +7,7 @@ module AST.Term.Scheme
     ( Scheme(..), sForAlls, sTyp
     , ForAlls(..), _ForAlls
     , schemeAsType
+    , loadScheme
 
     , QVarValues(..), _QVarValues
     , makeQVarValues
@@ -15,13 +16,14 @@ module AST.Term.Scheme
 import           Algebra.Lattice (JoinSemiLattice(..))
 import           AST.Class.Children (Children(..), ChildrenWithConstraint)
 import           AST.Class.Children.TH (makeChildren)
-import           AST.Class.Combinators (And)
+import           AST.Class.Combinators (And, NoConstraint, HasChildrenConstraint, proxyNoConstraint)
 import           AST.Class.HasChild (HasChild(..))
 import           AST.Class.Recursive (Recursive, wrapM)
 import           AST.Knot (Tree, Tie, RunKnot)
 import           AST.Knot.Pure (Pure(..))
 import           AST.Unify
 import           AST.Unify.Binding (Binding(..))
+import           AST.Unify.Generalize (Generalized(..), GTerm(..), _GMono)
 import           AST.Unify.Term (UTerm(..))
 import           Control.DeepSeq (NFData)
 import qualified Control.Lens as Lens
@@ -107,6 +109,42 @@ schemeAsType (Pure (Scheme vars typ)) =
     do
         foralls <- children (Proxy :: Proxy (Unify m)) makeQVarValues vars
         wrapM (Proxy :: Proxy (And (Unify m) (HasChild varTypes))) (schemeBodyToType foralls) typ
+
+loadBody ::
+    ( Unify m typ
+    , HasChild varTypes typ
+    , ChildrenConstraint typ NoConstraint
+    ) =>
+    Tree varTypes (QVarValues (UVar m)) ->
+    Tree typ (GTerm (UVar m)) ->
+    m (Tree (GTerm (UVar m)) typ)
+loadBody foralls x =
+    case x ^? quantifiedVar >>= getForAll of
+    Just r -> GPoly r & pure
+    Nothing ->
+        case children proxyNoConstraint (^? _GMono) x of
+        Just xm -> newTerm xm <&> GMono
+        Nothing -> GBody x & pure
+    where
+        getForAll v = foralls ^? getChild . _QVarValues . Lens.ix v
+
+-- | Load scheme into unification monad so that different instantiations share
+-- the scheme's monomorphic parts -
+-- their unification is O(1) as it is the same shared unification term.
+loadScheme ::
+    forall m varTypes typ.
+    ( Monad m
+    , ChildrenWithConstraint varTypes (Unify m)
+    , Recursive (Unify m `And` HasChild varTypes `And` HasChildrenConstraint NoConstraint) typ
+    ) =>
+    Tree Pure (Scheme varTypes typ) ->
+    m (Tree (Generalized typ) (UVar m))
+loadScheme (Pure (Scheme vars typ)) =
+    do
+        foralls <- children (Proxy :: Proxy (Unify m)) makeQVarValues vars
+        wrapM (Proxy :: Proxy (Unify m `And` HasChild varTypes `And` HasChildrenConstraint NoConstraint))
+            (loadBody foralls) typ
+            <&> Generalized
 
 type DepsS c v t k = ((c (Tree v ForAlls), c (Tie k t)) :: Constraint)
 deriving instance DepsS Eq   v t k => Eq   (Scheme v t k)
