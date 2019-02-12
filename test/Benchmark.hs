35dd82732b5bb9d4993223c3dcf717c36d232fb6
Eyal Lotem <eyal.lotem@gmail.com>
2019-02-12 17:01:04 +0200

    Add benchmark: large row unification

---
+++ test/Benchmark.hs
@@ -0,0 +1,31 @@
+import AST
+import AST.Unify
+import Control.Exception (evaluate)
+import Control.Lens.Operators
+import Criterion (Benchmarkable, whnfIO)
+import Criterion.Main (bench, defaultMain)
+import LangB
+import Text.PrettyPrint.HughesPJClass (prettyShow)
+import TypeLang.Pure
+
+fields :: [(String, Tree Pure Typ)]
+fields = [ ("a" ++ show i, Pure TInt) | i <- [0 :: Int .. 10] ]
+
+recordFwd :: Tree Pure Typ
+recordFwd = record fields
+
+recordBwd :: Tree Pure Typ
+recordBwd = record (reverse fields)
+
+unifyLargeRows :: Benchmarkable
+unifyLargeRows =
+    do
+        r1 <- unfreeze recordFwd
+        r2 <- unfreeze recordBwd
+        unify r1 r2
+        & execPureInferB
+        & either (fail . prettyShow) evaluate
+        & whnfIO
+
+main :: IO ()
+main = defaultMain [bench "Unify large rows" unifyLargeRows]
