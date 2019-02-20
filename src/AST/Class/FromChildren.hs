7a91c71882ab485e10d805c7c47134edd8b823c3
Yair Chuchem <yairchu@gmail.com>
2019-02-20 15:12:10 +0200

    FromChildren class

---
+++ src/AST/Class/FromChildren.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoImplicitPrelude, RankNTypes, ConstraintKinds #-}
+
+module AST.Class.FromChildren
+    ( FromChildren(..)
+    ) where
+
+import AST (Tree, ChildrenConstraint, Pure(..))
+import AST.Knot.Functor (ToKnot(..))
+import Control.Lens.Operators
+import Data.Proxy (Proxy)
+
+import Prelude.Compat
+
+class FromChildren parent where
+    fromChildren ::
+        (Applicative f, ChildrenConstraint parent constraint) =>
+        Proxy constraint ->
+        (forall child. constraint child => f (Tree k child)) ->
+        f (Tree parent k)
+
+instance FromChildren Pure where
+    fromChildren _ mk = mk <&> Pure
+
+instance Applicative f => FromChildren (ToKnot f) where
+    fromChildren _ mk = mk <&> pure <&> ToKnot
