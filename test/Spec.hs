lamdu/hypertypes

commit 2dfe61c373311046e037f6118cf6e30dadcff10e

Fix generalize getting stuck on infinite types, produces occurs error instead

src/AST/Unify/Generalize.hs
@@ -65,17 +65,14 @@ instance Children (Flip GTerm ast) where
             <&> GBody
         <&> Flip
 
--- | Generalize a unification term pointed by the given variable to a
--- `Generalized` term. Unification variables that are scoped within
--- the term become universally quantified skolems.
-generalize ::
+generalizeH ::
     forall m t.
     Recursive (Unify m) t =>
-    Tree (UVarOf m) t -> m (Tree (GTerm (UVarOf m)) t)
-generalize v0 =
+    Tree (UVarOf m) t -> WriterT [m ()] m (Tree (GTerm (UVarOf m)) t)
+generalizeH v0 =
     do
-        (v1, u) <- semiPruneLookup v0
-        c <- scopeConstraints
+        (v1, u) <- semiPruneLookup v0 & lift
+        c <- lift scopeConstraints
         case u of
             UUnbound l | l `leq` c ->
                 GPoly v1 <$
@@ -83,19 +80,34 @@ generalize v0 =
                 -- so additional unifications after generalization
                 -- (for example hole resumptions where supported)
                 -- cannot unify it with anything.
-                bindVar binding v1 (USkolem (generalizeConstraints l))
+                lift (bindVar binding v1 (USkolem (generalizeConstraints l)))
             USkolem l | l `leq` c -> pure (GPoly v1)
             UTerm t ->
                 withDict (recursive :: RecursiveDict (Unify m) t) $
-                children p generalize (t ^. uBody)
+                do
+                    tell [bindVar binding v1 (UTerm t)]
+                    bindVar binding v1 (UResolving t) & lift
+                    children p generalizeH (t ^. uBody)
                 <&>
                 \b ->
                 if foldMapChildren p (All . Lens.has _GMono) b ^. Lens._Wrapped
                 then GMono v1
                 else GBody b
+            UResolving t -> GMono v1 <$ lift (occursError v1 t)
             _ -> pure (GMono v1)
     where
         p = Proxy :: Proxy (Recursive (Unify m))
+
+-- | Generalize a unification term pointed by the given variable to a `GTerm`.
+-- Unification variables that are scoped within the term
+-- become universally quantified skolems.
+generalize ::
+    Recursive (Unify m) t =>
+    Tree (UVarOf m) t -> m (Tree (GTerm (UVarOf m)) t)
+generalize v =
+    do
+        (r, recover) <- runWriterT (generalizeH v)
+        r <$ sequence_ recover
 
 {-# INLINE instantiateForAll #-}
 instantiateForAll ::

test/Spec.hs
@@ -53,6 +53,9 @@ nomLam =
 letGen :: Tree Pure LangB
 letGen = bLet "id" (lam "x" id) \i -> i $$ i $$ bLit 5
 
+genInf :: Tree Pure LangB
+genInf = bLet "f" (lam "x" $ \x -> x $$ x) id
+
 shouldNotGen :: Tree Pure LangB
 shouldNotGen = lam "x" \x -> bLet "y" x id
 
@@ -252,6 +255,7 @@ main =
             , testA validForAll  "Right (t0 -> t0)"
             , testA nomLam       "Right (Map[key: Int, value: Int] -> Map[key: Int, value: Int])"
             , testB letGen       "Right Int"
+            , testB genInf       "Left (t0 occurs in itself, expands to: t0 -> t1)"
             , testB shouldNotGen "Right (t0 -> t0)"
             , testB simpleRec    "Right (a : Int :*: {})"
             , testB extendLit    "Left (Mismatch Int r0)"

src/AST/Unify.hs
@@ -8,7 +8,7 @@ module AST.Unify
     , module AST.Unify.QuantifiedVar
     , applyBindings, unify
     , semiPruneLookup
-    , newUnbound, newTerm, unfreeze
+    , newUnbound, newTerm, unfreeze, occursError
 
     , -- Exported for SPECIALIZE pragmas
       updateConstraints, updateTermConstraints

commit e935c41f5ba15be937801c88ff25ded5e2540600

Oops 2dfe61c37331 broke generalize with repeating terms in scheme.

src/AST/Term/Scheme.hs
@@ -189,6 +189,7 @@ saveH (GMono x) =
             <&>
             \case
             UTerm t -> t ^. uBody
+            UUnbound{} -> error "saveScheme of non-toplevel scheme!"
             _ -> error "unexpected state at saveScheme of monomorphic part"
 saveH (GPoly x) =
     lookupVar binding x & lift

src/AST/Unify/Generalize.hs
@@ -65,14 +65,17 @@ instance Children (Flip GTerm ast) where
             <&> GBody
         <&> Flip
 
-generalizeH ::
+-- | Generalize a unification term pointed by the given variable to a `GTerm`.
+-- Unification variables that are scoped within the term
+-- become universally quantified skolems.
+generalize ::
     forall m t.
     Recursive (Unify m) t =>
-    Tree (UVarOf m) t -> WriterT [m ()] m (Tree (GTerm (UVarOf m)) t)
-generalizeH v0 =
+    Tree (UVarOf m) t -> m (Tree (GTerm (UVarOf m)) t)
+generalize v0 =
     do
-        (v1, u) <- semiPruneLookup v0 & lift
-        c <- lift scopeConstraints
+        (v1, u) <- semiPruneLookup v0
+        c <- scopeConstraints
         case u of
             UUnbound l | l `leq` c ->
                 GPoly v1 <$
@@ -80,34 +83,23 @@ generalizeH v0 =
                 -- so additional unifications after generalization
                 -- (for example hole resumptions where supported)
                 -- cannot unify it with anything.
-                lift (bindVar binding v1 (USkolem (generalizeConstraints l)))
+                bindVar binding v1 (USkolem (generalizeConstraints l))
             USkolem l | l `leq` c -> pure (GPoly v1)
             UTerm t ->
                 withDict (recursive :: RecursiveDict (Unify m) t) $
                 do
-                    tell [bindVar binding v1 (UTerm t)]
-                    bindVar binding v1 (UResolving t) & lift
-                    children p generalizeH (t ^. uBody)
+                    bindVar binding v1 (UResolving t)
+                    r <- children p generalize (t ^. uBody)
+                    r <$ bindVar binding v1 (UTerm t)
                 <&>
                 \b ->
                 if foldMapChildren p (All . Lens.has _GMono) b ^. Lens._Wrapped
                 then GMono v1
                 else GBody b
-            UResolving t -> GMono v1 <$ lift (occursError v1 t)
+            UResolving t -> GMono v1 <$ occursError v1 t
             _ -> pure (GMono v1)
     where
         p = Proxy :: Proxy (Recursive (Unify m))
-
--- | Generalize a unification term pointed by the given variable to a `GTerm`.
--- Unification variables that are scoped within the term
--- become universally quantified skolems.
-generalize ::
-    Recursive (Unify m) t =>
-    Tree (UVarOf m) t -> m (Tree (GTerm (UVarOf m)) t)
-generalize v =
-    do
-        (r, recover) <- runWriterT (generalizeH v)
-        r <$ sequence_ recover
 
 {-# INLINE instantiateForAll #-}
 instantiateForAll ::

test/Spec.hs
@@ -50,11 +50,16 @@ nomLam =
             & Pure
             & uniType
 
-letGen :: Tree Pure LangB
-letGen = bLet "id" (lam "x" id) \i -> i $$ i $$ bLit 5
+letGen0 :: Tree Pure LangB
+letGen0 = bLet "id" (lam "x" id) \i -> i $$ i $$ bLit 5
+
+letGen1 :: Tree Pure LangB
+letGen1 =
+    bLet "five" (bLit 5) \five ->
+    bLet "f" (lam "x" \x -> x $$ five $$ five) id
 
 genInf :: Tree Pure LangB
-genInf = bLet "f" (lam "x" $ \x -> x $$ x) id
+genInf = bLet "f" (lam "x" \x -> x $$ x) id
 
 shouldNotGen :: Tree Pure LangB
 shouldNotGen = lam "x" \x -> bLet "y" x id
@@ -254,7 +259,8 @@ main =
             , testA skolem       "Left (SkolemEscape: t0)"
             , testA validForAll  "Right (t0 -> t0)"
             , testA nomLam       "Right (Map[key: Int, value: Int] -> Map[key: Int, value: Int])"
-            , testB letGen       "Right Int"
+            , testB letGen0      "Right Int"
+            , testB letGen1      "Right ((Int -> Int -> t0) -> t0)"
             , testB genInf       "Left (t0 occurs in itself, expands to: t0 -> t1)"
             , testB shouldNotGen "Right (t0 -> t0)"
             , testB simpleRec    "Right (a : Int :*: {})"