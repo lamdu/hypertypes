1a702457e30510022a545c4677eb6a4962e6c019
Yair Chuchem <yairchu@gmail.com>
2019-07-07 18:57:04 +0300

    Both: missing instances

--- src/AST/Knot/Both.hs
+++ src/AST/Knot/Both.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, TypeFamilies, DataKinds, TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude, TypeFamilies, DataKinds, TemplateHaskell, DeriveGeneric #-}
 
 module AST.Knot.Both
     ( Both(..), bothA, bothB
@@ -6,14 +6,19 @@ module AST.Knot.Both
 
 import AST.Class.Children.TH (makeChildren)
 import AST.Knot (Knot)
+import Control.DeepSeq (NFData)
 import Control.Lens (makeLenses)
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
 
 import Prelude.Compat
 
 data Both a b (k :: Knot) = Both
     { _bothA :: a k
     , _bothB :: b k
-    } deriving (Eq, Ord, Show)
+    } deriving (Eq, Ord, Show, Generic)
 makeLenses ''Both
-
 makeChildren ''Both
+
+instance (Binary (a k), Binary (b k)) => Binary (Both a b k)
+instance (NFData (a k), NFData (b k)) => NFData (Both a b k)
