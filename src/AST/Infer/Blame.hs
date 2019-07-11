635dee2c3ebf96094778dc720a2b87f2813b7dce
Yair Chuchem <yairchu@gmail.com>
2019-07-11 16:30:18 +0300

    AST.Infer.Blame: a type error blame assignment algorithm

---
+++ src/AST/Infer/Blame.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE NoImplicitPrelude, FlexibleContexts, ScopedTypeVariables, TupleSections #-}
+
+module AST.Infer.Blame
+    ( Blame(..), blame
+    ) where
+
+import AST
+import AST.Class.Recursive
+import AST.Infer
+import AST.Unify
+import AST.Unify.Lookup
+import AST.Unify.New
+import AST.Unify.Occurs
+import Control.Lens.Operators
+import Control.Monad.Except (MonadError(..))
+import Data.Foldable (traverse_)
+import Data.List (sortOn)
+import Data.Proxy
+
+import Prelude.Compat
+
+data Blame = Ok | TypeMismatch
+
+data PrepAnn m a = PrepAnn
+    { pAnn :: a
+    , pTryUnify :: m ()
+    , pFinalize :: m Blame
+    }
+
+prepare ::
+    forall err m exp a.
+    (MonadError err m, Recursive (Infer m) exp) =>
+    Tree (UVarOf m) (TypeOf exp) ->
+    Tree (Ann a) exp ->
+    m (Tree (Ann (PrepAnn m a)) exp)
+prepare typeFromAbove (Ann a x) =
+    inferBody
+    (recursiveOverChildren (Proxy :: Proxy (Infer m))
+        (\c ->
+            do
+                t <- newUnbound
+                prepare t c <&> (`InferredChild` t)
+            & InferChild
+        )
+        x)
+    <&>
+    \(InferRes xI t) ->
+    Ann PrepAnn
+    { pAnn = a
+    , pTryUnify =
+        do
+            _ <- unify typeFromAbove t
+            occursCheck t
+        & (`catchError` const (pure ()))
+    , pFinalize =
+        do
+            (t0, _) <- semiPruneLookup typeFromAbove
+            (t1, _) <- semiPruneLookup t
+            if t0 == t1
+                then pure Ok
+                else pure TypeMismatch
+    } xI
+
+-- | Assign blame for type errors, given a prioritisation for the possible blame positions.
+--
+-- The expected `MonadError` behavior is that catching errors rolls back their state changes
+-- (i.e `StateT s (Either e)` is ok but `EitherT e (State s)` is not)
+blame ::
+    (Ord priority, MonadError err m, Recursive (Infer m) exp, Recursive Children exp) =>
+    (a -> priority) ->
+    Tree (UVarOf m) (TypeOf exp) ->
+    Tree (Ann a) exp ->
+    m (Tree (Ann (Blame, a)) exp)
+blame order topLevelType e =
+    do
+        p <- prepare topLevelType e
+        p ^.. annotations & sortOn (order . pAnn) & traverse_ pTryUnify
+        annotations (\x -> pFinalize x <&> (, pAnn x)) p
