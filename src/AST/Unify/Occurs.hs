314a1edd9a70762946da485cb0cfbe6c0c8d50f6
Yair Chuchem <yairchu@gmail.com>
2019-07-11 14:51:45 +0300

    add occursCheck
    Previously `applyBindings` had to be used for performing occurs check,
    but it also changed the binding state by quantifying free unification variable.
    `occursCheck` does only the check without changing state.

--- src/AST/Unify/Occurs.hs
+++ src/AST/Unify/Occurs.hs
@@ -1,15 +1,20 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, ScopedTypeVariables, FlexibleContexts #-}
 
 module AST.Unify.Occurs
-    ( occursError
+    ( occursCheck
+    , -- Helper used for fused occurs-check in unification and apply bindings
+      occursError
     ) where
 
-import AST (Tree, _Pure)
+import AST
 import AST.Class.Unify (Unify(..), UVarOf, BindingDict(..))
+import AST.Class.Recursive (recursiveChildren_)
 import AST.Unify.Error (UnifyError(..))
+import AST.Unify.Lookup (semiPruneLookup)
 import AST.Unify.QuantifiedVar (HasQuantifiedVar(..), MonadQuantify(..))
-import AST.Unify.Term (UTerm(..), UTermBody(..))
+import AST.Unify.Term (UTerm(..), UTermBody(..), uBody)
 import Control.Lens.Operators
+import Data.Proxy (Proxy(..))
 
 import Prelude.Compat
 
@@ -21,3 +26,29 @@ occursError v (UTermBody c b) =
         q <- newQuantifiedVariable c
         bindVar binding v (UResolved (_Pure . quantifiedVar # q))
         unifyError (Occurs (quantifiedVar # q) b)
+
+{-# INLINE occursCheck #-}
+occursCheck ::
+    forall m t. Recursive (Unify m) t =>
+    Tree (UVarOf m) t -> m ()
+occursCheck v0 =
+    semiPruneLookup v0
+    >>=
+    \(v1, x) ->
+    case x of
+    UResolving t -> occursError v1 t
+    UResolved{} -> pure ()
+    UUnbound{} -> pure ()
+    USkolem{} -> pure ()
+    UTerm b ->
+        case (leafExpr :: Maybe (Tree t Pure -> Tree t Pure)) of
+        Just{} -> pure () -- no children to check!
+        Nothing ->
+            do
+                bindVar binding v1 (UResolving b)
+                recursiveChildren_ (Proxy :: Proxy (Unify m)) occursCheck
+                    (b ^. uBody)
+                bindVar binding v1 (UTerm b)
+    UToVar{} -> error "lookup not expected to result in var (in occursCheck)"
+    UConverted{} -> error "conversion state not expected in occursCheck"
+    UInstantiated{} -> error "occursCheck during instantiation"
