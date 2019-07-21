2b8b9779b469820c97a06b823acd6b544140aeec
Yair Chuchem <yairchu@gmail.com>
2019-07-21 18:18:26 +0300

    remove dead code

--- src/AST/Class/Combinators.hs
+++ src/AST/Class/Combinators.hs
@@ -6,18 +6,13 @@
 
 module AST.Class.Combinators
     ( And
-    , HasChildrenConstraint
     , TieHasConstraint
     ) where
 
-import AST.Class.Children (Children(..))
 import AST.Knot (Knot, Tie)
 
 class    (c0 k, c1 k) => And c0 c1 (k :: Knot -> *)
 instance (c0 k, c1 k) => And c0 c1 k
-
-class    ChildrenConstraint k c => HasChildrenConstraint c k
-instance ChildrenConstraint k c => HasChildrenConstraint c k
 
 class    constraint (Tie outer k) => TieHasConstraint constraint outer k
 instance constraint (Tie outer k) => TieHasConstraint constraint outer k
