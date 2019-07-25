2087f75a96ea232f2c23cc63ff1d3179bd0fe147
Yair Chuchem <yairchu@gmail.com>
2019-07-25 18:01:41 +0300

    AST.Constraint defunctionalized type-familiess for use by KLiftConstraint

---
+++ src/AST/Constraint.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE NoImplicitPrelude, TypeFamilies, DataKinds, TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ConstraintKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module AST.Constraint
+    ( KnotConstraintFunc(..)
+    , KnotsConstraint
+    , ConcatKnotConstraints
+    ) where
+
+import AST.Knot (Knot)
+import Data.Constraint (Constraint, Dict(..), withDict)
+import Data.Kind (Type)
+import Data.Proxy (Proxy(..))
+
+type KnotCons = (Knot -> Type) -> Constraint
+
+class KnotConstraintFunc (cls :: KnotCons -> Constraint) where
+    type family ApplyKnotConstraint cls (c :: KnotCons) :: Constraint
+    applyKnotConstraint ::
+        cls c =>
+        Proxy cls ->
+        Proxy c ->
+        Dict (ApplyKnotConstraint cls c)
+
+-- | Apply a constraint on the given knots
+class KnotsConstraint (ks :: [Knot -> Type]) c where
+    knotsConstraint ::
+        Proxy ks ->
+        Proxy c ->
+        Dict (ApplyKnotConstraint (KnotsConstraint ks) c)
+
+instance KnotConstraintFunc (KnotsConstraint '[]) where
+    type ApplyKnotConstraint (KnotsConstraint '[]) c = ()
+    {-# INLINE applyKnotConstraint #-}
+    applyKnotConstraint _ = knotsConstraint (Proxy :: Proxy ('[] :: [Knot -> Type]))
+
+instance KnotConstraintFunc (KnotsConstraint (k ': ks)) where
+    type ApplyKnotConstraint (KnotsConstraint (k ': ks)) c =
+            ( c k
+            , ApplyKnotConstraint (KnotsConstraint ks) c
+            )
+    {-# INLINE applyKnotConstraint #-}
+    applyKnotConstraint _ = knotsConstraint (Proxy :: Proxy (k ': ks))
+
+instance KnotsConstraint '[] c where
+    {-# INLINE knotsConstraint #-}
+    knotsConstraint _ _ = Dict
+
+instance (c k, KnotsConstraint ks c) => KnotsConstraint (k ': ks) c where
+    {-# INLINE knotsConstraint #-}
+    knotsConstraint _ p =
+        withDict (knotsConstraint (Proxy :: Proxy ks) p) Dict
+
+class ConcatKnotConstraints (xs :: [KnotCons -> Constraint]) c where
+    concatKnotConstraints ::
+        Proxy xs ->
+        Proxy c ->
+        Dict (ApplyKnotConstraint (ConcatKnotConstraints xs) c)
+
+instance KnotConstraintFunc (ConcatKnotConstraints '[]) where
+    type ApplyKnotConstraint (ConcatKnotConstraints '[]) c = ()
+    {-# INLINE applyKnotConstraint #-}
+    applyKnotConstraint _ =
+        concatKnotConstraints (Proxy :: Proxy ('[] :: [KnotCons -> Constraint]))
+
+instance KnotConstraintFunc (ConcatKnotConstraints (x ': xs)) where
+    type ApplyKnotConstraint (ConcatKnotConstraints (x ': xs)) c =
+        ( ApplyKnotConstraint x c
+        , ApplyKnotConstraint (ConcatKnotConstraints xs) c
+        )
+    {-# INLINE applyKnotConstraint #-}
+    applyKnotConstraint _ = concatKnotConstraints (Proxy :: Proxy (x ': xs))
+
+instance ConcatKnotConstraints '[] c where
+    {-# INLINE concatKnotConstraints #-}
+    concatKnotConstraints _ _ = Dict
+
+instance
+    (KnotConstraintFunc x, x c, ConcatKnotConstraints xs c) =>
+    ConcatKnotConstraints (x ': xs) c where
+
+    {-# INLINE concatKnotConstraints #-}
+    concatKnotConstraints _ p =
+        withDict (applyKnotConstraint (Proxy :: Proxy x) p)
+        (withDict (concatKnotConstraints (Proxy :: Proxy xs) p) Dict)
