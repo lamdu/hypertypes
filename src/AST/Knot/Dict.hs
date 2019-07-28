a8b4578d30cc290301e45a50d53b13d1aeb55ca1
Yair Chuchem <yairchu@gmail.com>
2019-07-28 19:20:11 +0300

    mapKWithDict

--- src/AST/Knot/Dict.hs
+++ src/AST/Knot/Dict.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE NoImplicitPrelude, RankNTypes, TypeFamilies, TypeOperators, DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module AST.Knot.Dict
     ( KDict(..), _KDict
     , ApplyKConstraints
-    , pureKWithDict
+    , pureKWithDict, mapKWithDict
     ) where
 
 import AST.Class
@@ -11,6 +12,7 @@ import AST.Knot (Tree, Knot, RunKnot)
 import Control.Lens (Iso, iso)
 import Data.Constraint (Dict(..), Constraint, withDict)
 import Data.Kind (Type)
+import Data.Proxy (Proxy(..))
 
 type family ApplyKConstraints (k :: Knot -> Type) cs :: Constraint where
     ApplyKConstraints k (c ': cs) = (c k, ApplyKConstraints k cs)
@@ -33,3 +35,15 @@ pureKWithDict ::
     (forall child. ApplyKConstraints child constraints => Tree n child) ->
     Tree k n
 pureKWithDict c f = mapK (\(MkKDict d) -> withDict d f) c
+
+{-# INLINE mapKWithDict #-}
+mapKWithDict ::
+    forall k m n constraints.
+    KFunctor k =>
+    Tree (NodeTypesOf k) (KDict constraints) ->
+    (forall child. ApplyKConstraints child constraints => Tree m child -> Tree n child) ->
+    Tree k m ->
+    Tree k n
+mapKWithDict c f =
+    withDict (hasNodes (Proxy :: Proxy k))
+    (mapC (pureKWithDict c (MkMapK f)))
