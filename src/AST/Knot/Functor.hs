d0dd585ac76de435c55f82d72d1ff3d797f3409d
Yair Chuchem <yairchu@gmail.com>
2019-08-30 16:56:37 +0300

    Rename AST.Knot.Functor.{ToKnot=>F}
    Where this knot may be used would be very verbose with the previous name.

--- src/AST/Knot/Functor.hs
+++ src/AST/Knot/Functor.hs
@@ -1,7 +1,7 @@
 -- | Functors as Knots
 {-# LANGUAGE TemplateHaskell, FlexibleInstances, UndecidableInstances #-}
 module AST.Knot.Functor
-    ( ToKnot(..), _ToKnot
+    ( F(..), _F
     ) where
 
 import AST.Class.Recursive
@@ -13,20 +13,20 @@ import GHC.Generics (Generic)
 
 import Prelude.Compat
 
-newtype ToKnot f k = MkToKnot (f (Node k (ToKnot f)))
+newtype F f k = F (f (Node k (F f)))
     deriving stock Generic
 
-_ToKnot ::
-    Iso (Tree (ToKnot f0) k0)
-        (Tree (ToKnot f1) k1)
-        (f0 (Tree k0 (ToKnot f0)))
-        (f1 (Tree k1 (ToKnot f1)))
-_ToKnot = iso (\(MkToKnot x) -> x) MkToKnot
+_F ::
+    Iso (Tree (F f0) k0)
+        (Tree (F f1) k1)
+        (f0 (Tree k0 (F f0)))
+        (f1 (Tree k1 (F f1)))
+_F = iso (\(F x) -> x) F
 
-makeCommonInstances [''ToKnot]
-makeKTraversableApplyAndBases ''ToKnot
+makeCommonInstances [''F]
+makeKTraversableApplyAndBases ''F
 
-instance RNodes (ToKnot f)
-instance Functor f => RFunctor (ToKnot f)
-instance Foldable f => RFoldable (ToKnot f)
-instance Traversable f => RTraversable (ToKnot f)
+instance RNodes (F f)
+instance Functor f => RFunctor (F f)
+instance Foldable f => RFoldable (F f)
+instance Traversable f => RTraversable (F f)
