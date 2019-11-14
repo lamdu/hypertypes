bf2b6f9613a188b2ebd0c761937d078deddefaec
Yair Chuchem <yairchu@gmail.com>
2019-11-14 22:44:13 +0200

    HContext - a class for hypertypes which can be "edited" by replacing their nodes

---
+++ src/Hyper/Class/Context.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE FlexibleContexts, ScopedTypeVariables, TypeApplications #-}
+
+module Hyper.Class.Context
+    ( HContext(..)
+    , recursiveContexts, recursiveContextsWith
+    ) where
+
+import Data.Constraint (withDict)
+import Data.Proxy (Proxy(..))
+import Hyper
+import Hyper.Type.Cont (HCont(..))
+
+import Prelude.Compat
+
+class HContext h where
+    hcontext ::
+        Tree h p ->
+        Tree h (HCont (Tree h p) p :*: p)
+
+recursiveContexts ::
+    (Recursively HContext h, Recursively HFunctor h) =>
+    Tree (Ann p) h ->
+    Tree (Ann (HCont (Tree (Ann p) h) p :*: p)) h
+recursiveContexts = recursiveContextsWith . (HCont id :*:)
+
+recursiveContextsWith ::
+    forall h p r.
+    (Recursively HContext h, Recursively HFunctor h) =>
+    Tree (HCont r (Ann p) :*: Ann p) h ->
+    Tree (Ann (HCont r p :*: p)) h
+recursiveContextsWith (HCont s0 :*: Ann a b) =
+    withDict (recursively (Proxy @(HContext h))) $
+    withDict (recursively (Proxy @(HFunctor h)))
+    Ann
+    { _hAnn = HCont (s0 . (`Ann` b)) :*: a
+    , _hVal =
+        hmap
+        ( Proxy @(Recursively HContext) #*# Proxy @(Recursively HFunctor) #>
+            \(HCont s1 :*: x) ->
+            recursiveContextsWith (HCont (s0 . Ann a . s1) :*: x)
+        ) (hcontext b)
+    }
---
+++ src/Hyper/Type/Cont.hs
@@ -0,0 +1,15 @@
+module Hyper.Type.Cont
+    ( HCont(..)
+    ) where
+
+import Control.Lens (Iso, iso)
+import Hyper (HyperType, Tree)
+
+newtype HCont r (p :: HyperType) h = HCont (p h -> r)
+
+_HCont ::
+    Iso (Tree (HCont r0 p0) h0)
+        (Tree (HCont r1 p1) h1)
+        (Tree p0 h0 -> r0)
+        (Tree p1 h1 -> r1)
+_HCont = iso (\(HCont x) -> x) HCont
